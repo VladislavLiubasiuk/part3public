69c698003d36327c37ee7c0b019eb84eccb1b38a
VladislavLiubasiuk <55456465+VladislavLiubasiuk@users.noreply.github.com>
2021-03-30 18:29:52 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,11 +5,5 @@ def test_if_button_is_present(browser):
     browser.implicitly_wait(5)
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    looking = browser.find_element_by_class_name("no-js")
-    user_language = looking.get_attribute("lang")
-    if user_language == "es":
-        result1 = browser.find_element_by_class_name("btn.btn-lg.btn-primary").text
-        assert result1 == "Añadir al carrito", "WRONG TEXT!"
-    elif user_language == "fr":
-        result1 = browser.find_element_by_class_name("btn.btn-lg.btn-primary").text
-        assert result1 == "Ajouter au panier", "WRONG TEXT!"
+    looking = browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert looking == "btn.btn-lg.btn-primary.btn-add-to-basket", "No button present!"
