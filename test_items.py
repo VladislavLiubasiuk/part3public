1424909bb48ec3f666db2727fc4205fb6a3c8b98
VladislavLiubasiuk <55456465+VladislavLiubasiuk@users.noreply.github.com>
2021-03-30 18:39:56 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,9 +1,9 @@
-import pytest
-from selenium import webdriver
+import time
 
 def test_if_button_is_present(browser):
     browser.implicitly_wait(5)
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    #time.sleep(30)
     browser.get(link)
-    looking = browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket")
-    assert looking == "btn.btn-lg.btn-primary.btn-add-to-basket", "No button present!"
+    looking = browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert looking, "No button present!"
